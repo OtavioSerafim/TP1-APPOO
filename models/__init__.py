208b151ee29c4f1648f6660a48c28ae695718277
Otávio Serafim <otavio.serafim2002@gmail.com>
2025-10-21 17:16:14 -0300

    adiciona arquivos de inicialização para modelos com importações

---
+++ models/__init__.py
@@ -0,0 +1,23 @@
+"""Exposição simples de modelos para importações no estilo Django."""
+
+from .main import (
+    Model,
+    Usuario,
+    Aluno,
+    Personal,
+    Gestor,
+    Equipamento,
+    Ficha,
+    Exercicio,
+)
+
+__all__ = [
+    "Model",
+    "Usuario",
+    "Aluno",
+    "Personal",
+    "Gestor",
+    "Equipamento",
+    "Ficha",
+    "Exercicio",
+]
