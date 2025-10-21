cf4e4259e9cd1ac58bc1593e0f9d804fa871ffa9
Otávio Serafim <otavio.serafim2002@gmail.com>
2025-10-21 14:23:32 -0300

    adiciona restrição de verificação para o tipo de usuário na tabela usuarios

--- database/migrations/1_usuarios.sql
+++ database/migrations/1_usuarios.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS usuarios (
     nome TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     senha TEXT NOT NULL,
-    tipo_usuario TEXT NOT NULL,
+    tipo_usuario TEXT NOT NULL CHECK (tipo_usuario IN ('gestor', 'personal', 'aluno')),
     criado_em TEXT NOT NULL DEFAULT (DATE('now')),
     atualizado_em TEXT NOT NULL DEFAULT (DATE('now'))
 );
