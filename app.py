2b4abf1ef5b471eee314325dc162200f5cf11046
germano-fagundes <germanomarquescfagundes@gmail.com>
2025-10-23 08:28:59 -0300

    configura inicialmente o banco de dados

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask
+from config import Config
 from controller.user_controller import UserController
 
 app = Flask(__name__, template_folder=os.path.join('views', 'templates'))
+app.config.from_object(Config)
 
 app.add_url_rule('/', 'index', UserController.index)
 
