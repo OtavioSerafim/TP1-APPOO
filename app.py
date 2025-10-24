274a021ca59468b1fe18a43ad964ee928abe8fb4
Otávio Serafim <otavio.serafim2002@gmail.com>
2025-10-24 16:36:02 -0300

    feat: adiciona gerenciamento de modelos com setup e teardown no Flask

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask
+from flask import Flask, g
+from models import Models
 from controller.user_controller import UserController
 
 app = Flask(
@@ -8,6 +9,17 @@ app = Flask(
     static_folder=os.path.join('views', 'static'),
     static_url_path='/static'
 )
+
+
+@app.before_request
+def setup_models():
+    g.models = Models()
+
+@app.teardown_request
+def teardown_models(exc=None):
+    models = getattr(g, "models", None)
+    if models is not None:
+        models.close()
 
 app.add_url_rule('/', 'login', UserController.login)
 app.add_url_rule('/cadastro', 'cadastro', UserController.cadastro)
