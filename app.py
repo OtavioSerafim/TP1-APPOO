193fc7e43d3076a4f14758bff5f9ca0b44a90cb8
Germano Fagundes <germanofagundes@ufmg.br>
2025-10-23 23:34:04 -0300

    atualiza rotas conforme as novas telas (home-gestor e equipamentos) criadas. atualiza regras gerais css e deleta index.html

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ app.config.from_object(Config)
 
 app.add_url_rule('/', 'login', UserController.login)
 app.add_url_rule('/cadastro', 'cadastro', UserController.cadastro)
+app.add_url_rule('/gestor', 'gestor', UserController.gestor)
+app.add_url_rule('/gestor/equipamentos', 'equipamentos', UserController.equipamentos)
 
 if (__name__) == '__main__':
     app.run(debug=True)
--- controller/user_controller.py
+++ controller/user_controller.py
@@ -8,4 +8,12 @@ class UserController:
     @staticmethod
     def cadastro():
         return render_template('cadastro.html')
+    
+    @staticmethod
+    def gestor():
+        return render_template('home-gestor.html')
+    
+    @staticmethod
+    def equipamentos():
+        return render_template('equipamentos.html')
 
